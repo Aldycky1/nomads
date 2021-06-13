d159974f8fc5627d5cddc9a9cc4b7e581cbc3f40
Aldycky1 <bagusaldycky@gmail.com>
2021-06-13 22:18:18 +0700

    Create Gallery Admin

--- resources/views/pages/admin/gallery/create.blade.php
+++ resources/views/pages/admin/gallery/create.blade.php
@@ -20,9 +20,9 @@
 
     <div class="card-shadow">
       <div class="card-body">
-        <form action="{{ route('travel-package.store') }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('gallery.store') }}" method="POST" enctype="multipart/form-data">
           @csrf
-          <div class="form-control">
+          <div class="form-group">
             <label for="travel_packages_id">Paket Travel</label>
             <select name="travel_packages_id" required class="form-control">
               <option value="">Pilih Paket Travel</option>
@@ -32,6 +32,10 @@
                   </option>
               @endforeach
             </select>
+          </div>
+          <div class="form-group">
+            <label for="image">Image</label>
+            <input type="file" class="form-control" name="image" placeholder="Image">
           </div>
           <button type="submit" class="btn btn-primary btn-block">
             Simpan
