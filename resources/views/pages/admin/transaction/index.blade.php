731144b8f9732932c781ec86dfc735e939183916
Aldycky1 <bagusaldycky@gmail.com>
2021-06-16 07:24:31 +0700

    Added Read and Detail

--- resources/views/pages/admin/transaction/index.blade.php
+++ resources/views/pages/admin/transaction/index.blade.php
@@ -5,10 +5,7 @@
   <div class="container-fluid">
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-      <h1 class="h3 mb-0 text-gray-800">Gallery</h1>
-      <a href="{{ route('gallery.create') }}" class="btn btn-sm btn-primary shadow-sm">
-        <i class="fas fa-plus fa-sm text-white-50">Tambah Gallery</i>
-      </a>
+      <h1 class="h3 mb-0 text-gray-800">Transaksi</h1>
     </div>
 
     <div class="row">
@@ -19,7 +16,10 @@
               <tr>
                 <th>ID</th>
                 <th>Travel</th>
-                <th>Gambar</th>
+                <th>User</th>
+                <th>Visa</th>
+                <th>Total</th>
+                <th>Status</th>
                 <th>Action</th>
               </tr>
             </thead>
@@ -28,14 +28,18 @@
                 <tr>
                   <td>{{ $item->id }}</td>
                   <td>{{ $item->travel_package->title }}</td>
+                  <td>{{ $item->user->name }}</td>
+                  <td>${{ $item->additional_visa }}</td>
+                  <td>${{ $item->transaction_total }}</td>
+                  <td>{{ $item->transaction_status }}</td>
                   <td>
-                    <img src="{{ Storage::url($item->image) }}" alt="" style="width: 150px" class="img-thumbnail" />
-                  </td>
-                  <td>
-                    <a href="{{ route('gallery.edit', $item->id) }}" class="btn btn-info">
+                    <a href="{{ route('transaction.show', $item->id) }}" class="btn btn-primary">
+                      <i class="fa fa-eye"></i>
+                    </a>
+                    <a href="{{ route('transaction.edit', $item->id) }}" class="btn btn-info">
                       <i class="fa fa-pencil-alt"></i>
                     </a>
-                    <form action="{{ route('gallery.destroy', $item->id) }}" method="POST" class="d-inline">
+                    <form action="{{ route('transaction.destroy', $item->id) }}" method="POST" class="d-inline">
                       @csrf
                       @method('delete')
                       <button class="btn btn-danger">
